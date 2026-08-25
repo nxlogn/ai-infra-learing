329fb73befa31e06f17fae5abddd2aba015901bb
nxlogn <553403830@qq.com>
2026-08-25 21:40:04 +0800

    feat:编译器设置

--- leetcode/23合并k个升序链表.cpp
+++ leetcode/23合并k个升序链表.cpp
@@ -1,8 +1,7 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-struct ListNode
-{
+struct ListNode {
   int val;
   ListNode *next;
   ListNode() : val(0), next(nullptr) {}
@@ -10,39 +9,30 @@ struct ListNode
   ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
-struct cmp
-{
-  bool operator()(ListNode *a, ListNode *b)
-  {
-    return a->val > b->val;
-  }
+struct cmp {
+  bool operator()(ListNode *a, ListNode *b) { return a->val > b->val; }
 };
-class Solution
-{
+class Solution {
 public:
-  ListNode *mergeKLists(vector<ListNode *> &lists)
-  {
+  ListNode *mergeKLists(vector<ListNode *> &lists) {
     // 最小堆
     priority_queue<ListNode *, vector<ListNode *>, cmp> pq;
 
     // 将每个链表的头节点加入堆
-    for (int i = 0; i < lists.size(); i++)
-    {
-      if (lists[i] != nullptr)
-      {
+    for (int i = 0; i < lists.size(); i++) {
+      if (lists[i] != nullptr) {
         pq.push(lists[i]);
       }
     }
 
     // 方便处理头节点
-    ListNode* dummy = new ListNode(-1);
-    ListNode* cur = dummy;
+    ListNode *dummy = new ListNode(-1);
+    ListNode *cur = dummy;
 
     // 不断取出当前最小
-    while(!pq.empty())
-    {
+    while (!pq.empty()) {
       // 取堆顶
-      ListNode* node = pq.top();
+      ListNode *node = pq.top();
       pq.pop();
 
       // 接到结果链表
@@ -50,8 +40,7 @@ public:
       cur = cur->next;
 
       // 如果当前节点后面还有节点，加入堆
-      if (node->next != nullptr)
-      {
+      if (node->next != nullptr) {
         pq.push(node->next);
       }
     }
@@ -61,12 +50,10 @@ public:
 };
 
 // 辅助函数：根据数组构造链表
-ListNode *createList(const vector<int> &vals)
-{
+ListNode *createList(const vector<int> &vals) {
   ListNode *dummy = new ListNode(-1);
   ListNode *cur = dummy;
-  for (int v : vals)
-  {
+  for (int v : vals) {
     cur->next = new ListNode(v);
     cur = cur->next;
   }
@@ -74,27 +61,23 @@ ListNode *createList(const vector<int> &vals)
 }
 
 // 辅助函数：打印链表
-void printList(ListNode *head)
-{
-  while (head != nullptr)
-  {
+void printList(ListNode *head) {
+  while (head != nullptr) {
     cout << head->val;
-    if (head->next) cout << "->";
+    if (head->next)
+      cout << "->";
     head = head->next;
   }
   cout << endl;
 }
 
-int main()
-{
+int main() {
   Solution s;
 
   // 测试用例1
   {
-    vector<ListNode *> lists = {
-        createList({1, 4, 5}),
-        createList({1, 3, 4}),
-        createList({2, 6})};
+    vector<ListNode *> lists = {createList({1, 4, 5}), createList({1, 3, 4}),
+                                createList({2, 6})};
     cout << "用例1: ";
     printList(s.mergeKLists(lists));
   }
